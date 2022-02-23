55dae55fab86cb867ce88999fd7ed6ada8480b0b
Guriev.M <treil911@gmail.com>
2022-02-23 14:35:34 +0300

    added assets, changed some configs

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -32,6 +32,74 @@ module.exports = {
                     "sass-loader",
                 ],
             },
+            // {
+            //     test: /\.(png|svg|jpg|jpeg|gif|webp|mp4)$/i,
+            //     type: "asset/resource",
+            //     generator: {
+            //         filename: (pathData) => {
+            //             return pathData.filename.substr(4)
+            //         },
+            //     },
+            //     use: [
+            //         {
+            //             loader: "image-webpack-loader",
+            //             options: {
+            //                 mozjpeg: {
+            //                     progressive: true,
+            //                     quality: 70,
+            //                 },
+            //                 pngquant: {
+            //                     quality: [0.6, 0.9],
+            //                     speed: 1,
+            //                 },
+            //                 svgo: {
+            //                     removeTitle: true,
+            //                     convertColors: true,
+            //                     convertPathData: false,
+            //                 },
+            //                 gifsicle: {
+            //                     interlaced: false,
+            //                 },
+            //             },
+            //         },
+            //     ],
+            // },
+            // {
+            //     test: /\.(gif|png|jpe?g|svg)$/i,
+            //     use: [
+            //         'file-loader',
+            //         {
+            //             loader: 'image-webpack-loader',
+            //             options: {
+            //                 name: 'dirname/[contenthash].[ext]',
+            //                 mozjpeg: {
+            //                     progressive: true,
+            //                 },
+            //                 // optipng.enabled: false will disable optipng
+            //                 optipng: {
+            //                     enabled: false,
+            //                 },
+            //                 pngquant: {
+            //                     quality: [0.65, 0.90],
+            //                     speed: 4
+            //                 },
+            //                 gifsicle: {
+            //                     interlaced: false,
+            //                 },
+            //                 // the webp option will enable WEBP
+            //                 webp: {
+            //                     quality: 75
+            //                 }
+            //             }
+            //         },
+            //     ],
+            // }
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    'file-loader'
+                ],
+            },
         ],
     },
     plugins: [
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -46,6 +46,80 @@ module.exports = {
                     "sass-loader",
                 ],
             },
+            // {
+            //     test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            //     type: "asset/resource",
+            //     generator: {
+            //         filename: (pathData) => {
+            //             return pathData.filename.substr(4)
+            //         },
+            //     },
+            //     use: [
+            //         {
+            //             loader: "image-webpack-loader",
+            //             options: {
+            //                 mozjpeg: {
+            //                     progressive: true,
+            //                     quality: 70,
+            //                 },
+            //                 pngquant: {
+            //                     quality: [0.6, 0.9],
+            //                     speed: 1,
+            //                 },
+            //                 svgo: {
+            //                     removeTitle: true,
+            //                     convertColors: true,
+            //                     convertPathData: false,
+            //                 },
+            //                 gifsicle: {
+            //                     interlaced: false,
+            //                 },
+            //             },
+            //         },
+            //     ],
+            // },
+            // {
+            //     test: /\.(gif|png|jpe?g|svg)$/i,
+            //     use: [
+            //         'file-loader',
+            //         {
+            //             loader: 'image-webpack-loader',
+            //             options: {
+            //                 mozjpeg: {
+            //                     progressive: true,
+            //                 },
+            //                 // optipng.enabled: false will disable optipng
+            //                 optipng: {
+            //                     enabled: false,
+            //                 },
+            //                 pngquant: {
+            //                     quality: [0.65, 0.90],
+            //                     speed: 4
+            //                 },
+            //                 gifsicle: {
+            //                     interlaced: false,
+            //                 },
+            //                 // the webp option will enable WEBP
+            //                 webp: {
+            //                     quality: 75
+            //                 }
+            //             }
+            //         },
+            //     ],
+            // }
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            }
         ],
     },
     plugins: [
