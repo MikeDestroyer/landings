b045ea6120bdb7bfd876caeea51782d55888832b
Guriev.M <treil911@gmail.com>
2022-03-11 15:43:21 +0300

    added addded scrolling to position function

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -38,11 +38,17 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpe?g|gif)$/,
-                type: 'asset/resource'
+                type: 'asset/resource',
+                generator: {
+                    filename: 'media/[hash][ext][query]'
+                }
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/inline",
+                type: "asset/resource",
+                generator: {
+                    filename: 'fonts/[hash][ext][query]'
+                }
             },
         ],
     },
